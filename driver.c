5f1326ccffab738367cb1cbc286fa4a448255736
Tomas Menchaca <atm7812@mavs.uta.edu>
2025-06-14 18:24:01 -0500

    finished implementing sorting logic

--- driver.c
+++ driver.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
-
+#define SIZE 10000
 
 
 
 
 int main(){
 
+    int nums[SIZE];
+    char* names[SIZE];
 
 
+    for(int i = 0; i < SIZE - 1; i++){
+        for(int j = i + 1; j < SIZE; j++){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+
+            char* tempStr = names[i];
+            names[i] = names[j];
+            names[j] = tempStr;
+
+
+        }
+    }
 
     return 0;
 }
